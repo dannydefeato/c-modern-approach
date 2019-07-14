8ffe944a797d465edff63aa3faffa3e95325cc38
Danny Defeato <dannydefeato@protonmail.com>
2019-07-14 18:04:38 +0200

    ch06 pr12

--- Programming Projects/C6/12.c
+++ Programming Projects/C6/12.c
@@ -17,7 +17,25 @@
 
 int main(void){
 
- 
+    //int n;
+    float f = 1.00f, e = 1.00f, eps = 1.00f, t = 1.00f;
+
+    printf("Approximation of e using e = 1/n1!+ 1/n2! + ... + 1/n!\n");
+    printf("Enter limit of term (epsilon): "); scanf("%f", &eps);
+
+    for(int i = 1;; i++)
+    {
+        if(t >= eps)
+        {
+        t = 1/(f *= i);
+        e += t;
+        printf("Term %3d:\t%f\n", i, t);
+        }
+        else if( t < eps)
+        break;
+    }
+
+    printf("\nThe approximation of e (epsilon = %.4f) is: %.12f", eps, e);
 
     return 0;
 }
