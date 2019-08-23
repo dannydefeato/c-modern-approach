9c18d42a36c8c9183338836f8afcb1592da4e0c5
Danny Defeato <dannydefeato@protonmail.com>
2019-08-23 13:40:18 +0200

    ch07pr06 skeleton

---
+++ Programming Projects/C07/06.c
@@ -0,0 +1,12 @@
+//  C Programming - A Modern Approach (Second Edition)
+//  by K. N. King
+//  ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//  Chapter 7   Basic Types
+//              PROGRAMMING PROJECTS
+//              PROJECT No. 6 | p.158
+
+
+/* 
+    Write a program that prints the values of sizeof(int), sizeof(short),
+    sizeof(long), sizeof(float), sizeof(double) and sizeof(long double).    
+*/
