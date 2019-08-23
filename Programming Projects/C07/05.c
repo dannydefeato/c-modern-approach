d94d0c57108696ada865bf1ce367256bca584797
Danny Defeato <dannydefeato@protonmail.com>
2019-08-23 13:40:12 +0200

    ch07pr05 skeleton

---
+++ Programming Projects/C07/05.c
@@ -0,0 +1,22 @@
+//  C Programming - A Modern Approach (Second Edition)
+//  by K. N. King
+//  ||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//  Chapter 7   Basic Types
+//              PROGRAMMING PROJECTS
+//              PROJECT No. 5 | p.158
+
+
+/* 
+    In the SCRABBLE Crossword Game, players form words using small tiles, each containing
+    a letter and a face value. The face value varies from one letter to another, based on
+    the letter's rarity. (Here are the face values: 1: AEILNORSTU, 2: DG, 3: BCMP, 4: FHVWY,
+    5:K, 8: JX, 10: QZ.) Write a program that computes the value of a word by summing the
+    values of its letters:
+
+        Enter a word: pitfall
+        Scrabble value: 12
+
+    Your program should allow any mixture of lower-case and upper-case letters in the word.
+    Hint: use the toupper library function.
+    
+*/
