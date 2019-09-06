52731de80debf4a763473773d55a654a411fbaf3
Danny Defeato <dannydefeato@protonmail.com>
2019-09-06 16:52:02 +0200

    ch07pr05

--- Programming Projects/C07/05.c
+++ Programming Projects/C07/05.c
@@ -20,3 +20,62 @@
     Hint: use the toupper library function.
     
 */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+
+    char ch;
+    int score= 0;
+
+    printf ("Enter a word: ");
+
+    while ((ch = getchar()) != '\n')
+    {
+        switch (toupper (ch)){
+
+            case 'D':
+            case 'G':
+                        score += 1;
+                        break;
+
+            case 'B':
+            case 'C':
+            case 'M':
+            case 'P':
+                        score += 3;
+                        break;
+            
+            case 'F':
+            case 'H':
+            case 'V':
+            case 'W':
+            case 'Y':
+                        score += 4;
+                        break;
+
+            case 'K':
+                        score += 5;
+                        break;
+
+            case 'J':
+            case 'X':
+                        score += 8;
+                        break;
+
+            case 'Q':
+            case 'Z':
+                        score += 10;
+                        break;
+
+            default:    score += 1;
+                        break;
+        }
+    }
+    printf ("Scrabble value: %d", score);
+
+
+    return 0;
+}
