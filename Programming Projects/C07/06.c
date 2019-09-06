91f42a8f8f7818d634ba3711441707f02fac1976
Danny Defeato <dannydefeato@protonmail.com>
2019-09-06 16:52:11 +0200

    ch07pr06

--- Programming Projects/C07/06.c
+++ Programming Projects/C07/06.c
@@ -10,3 +10,17 @@
     Write a program that prints the values of sizeof(int), sizeof(short),
     sizeof(long), sizeof(float), sizeof(double) and sizeof(long double).    
 */
+
+#include <stdio.h>
+
+int main(void){
+
+    printf ("\nInt:\t\t%2d Bytes\n", sizeof(int));
+    printf ("Short:\t\t%2d Bytes\n", sizeof(short));
+    printf ("Long:\t\t%2d Bytes\n", sizeof(long));
+    printf ("Float:\t\t%2d Bytes\n", sizeof(float));
+    printf ("Double:\t\t%2d Bytes\n", sizeof(double));
+    printf ("Long Double:\t%d Bytes\n", sizeof(long double));
+
+    return 0;
+}
